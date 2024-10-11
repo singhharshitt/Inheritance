415594b7532fb3a921e619fff3ba37634513d7e9
Harshit singh <149008610+singhharshitt@users.noreply.github.com>
2024-10-11 14:21:04 +0530

    Create MultipleInheritanceBasic.cpp

---
+++ MultipleInheritanceBasic.cpp
@@ -0,0 +1,37 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class M{
+    protected:
+    int m;
+    public:
+        void get_m(int x){
+            m=x;
+        }
+};
+class N{
+    protected:
+    int n;
+    public:
+        void get_n(int y){
+            n= y;
+        }
+};
+
+
+class P: public M, public N{
+    public:
+    void display(void){
+        cout<<"m = "<<m<<endl;
+        cout<<"n = "<<n<<endl;
+        cout<<"m * n ="<< m * n<< endl;
+    }
+};
+
+int main(){
+    P p;
+    p.get_m(10);
+    p.get_n(20);
+    p.display();
+    return 0;
+}
